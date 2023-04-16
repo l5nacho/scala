d7ea6bdf371e2acd0f268b5b53654734dba7d195
nacho <ignacio.contrerascabello@gmail.com>
2023-04-16 12:45:53 +0200

    Fin projecto classes

--- project4.scala
+++ project4.scala
@@ -5,12 +5,12 @@ import scala.util.control.Breaks._
 object main{
 
 class Producto(var name: String, var prize: Int, var count: Int) {
-  
-  def 
 
+  var totalPrize = prize * count
 
   def printData() : Unit = {
     println("Created product with name " + name + " prize " + prize + " and quantity " + count)
+    println("The total prize is " + totalPrize)
 	}
 }
 
@@ -18,35 +18,60 @@ def main(args: Array[String]) : Unit = {
 
   var bufferList = ListBuffer[Producto]();
   var option: String = " ";
-  var compra: String = "continuar"
+  var compra: String = "continuar";
+  var totalBill: Int = 0;
+
+  showOptions();
+	
+  do {
+		
+  option = readLine("Por favor introduce la opcion que quieres: ");
+		
+  if (option == "1") {
     
+    var product: Producto = createProduct();
+
+    //product.printData()
+    bufferList += product;
+    }
+		
+  else {
+    compra = "cerrar";
+      }
+
+    }
+
+  while (compra != "cerrar"); 
+  
+  for (w <- bufferList) {
+    w.printData();
+    totalBill += w.totalPrize;
+      }
+  println("The total bill is " + totalBill);
+    }
+
+  def showOptions(): Unit = {
+
   println("Elige una opcion: ")
   println("1. Introduce un producto: ")	
   println("2. Salir")
-		
-  while (compra != "cerrar") {
-		
-  option = readLine("Por favor introduce la opcion que quieres: ")
-		
-  if (option == "1") {
-    var n = readLine("Introduce el nombre del producto: ");
-    var p = readLine("Introduce el precio del producto: ").toInt;
-    var q = readLine("Introduce la cantidad de productos" ).toInt; 
-		
-    var product = new Producto(n, p, q)
-    product.printData()
-    bufferList += product
-    }
-		
-    else {
-      compra = "cerrar";
-    }
-  }
-  for (w <- bufferList) {
-    w.printData()
-    }
 	
   }
+
+  def createProduct(): Producto = {
+
+    var n = readLine("Introduce el nombre del producto: ");
+    var p = readLine("Introduce el precio del producto: ").toInt;
+    var q = readLine("Introduce la cantidad de productos: " ).toInt; 
+    
+    var product = new Producto(n, p, q);
+
+    return product
+ 
+  }
+
+
+  
 
 }
 
